9351e407f8a647f80982f16704dc4525bc16bc23
Kebron Mehari <kebronmehari400@gmail.com>
2022-03-13 22:24:43 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b){
+    console.log (a, b)
+    return a + b;
+}
+function subtract(a, b){
+    console.log(a, b)
+    return a - b;
+}
+function multiply(a, b){
+    console.log(a, b)
+    return a * b;
+}
+function divide(a, b){
+    console.log(a, b)
+    return a / b;
+}   
+let n ='10'
+function increment(n){
+    console.log(n)
+    return n+=1;
+}
+function decrement(n){
+    return n-=1;
+}
+function makeInt(n){
+    return parseInt(n,10);
+}
+function preserveDecimal(n){
+    console.log(n)
+}
